150d4a5c33309b723e000e92682cc88b822e3d5a
Chiara Orvati <chiara.orvati@epfl.ch>
2017-04-05 14:25:52 +0200

    start own animalVision
    start my own implementation of rgbToSpectrum, basing myself on paper
    „rgbToSpectrum for Reflectances“

---
+++ AnimalVision_matlab/rgb2spectrum/imgToSpectrum.m
@@ -0,0 +1,8 @@
+function img = imgToSpectrum(in)
+
+[a b c] = size(in);
+img = zeros(a,b, 10); %for every pixel we will have its spectrum representation
+
+
+
+end
---
+++ AnimalVision_matlab/rgb2spectrum/rgb2spectrum.m
@@ -0,0 +1,28 @@
+close all;
+clear all;
+
+%rgb values
+white =     [1 1 1]';
+cyan =      [0 1 1]';
+magenta =   [1 0 1]';
+yellow =    [1 1 0]';
+red =       [1 0 0]';
+green =     [0 1 0]';
+blue =      [0 0 1]';
+
+% % sanity check :works!  
+% whiteSpectrum = toSpectrum(1,1,1);
+% cyanS =         toSpectrum(0,1,1);
+% magentaS =      toSpectrum(1,0,1);
+% yellowS =       toSpectrum(1,1,0);
+% redS =          toSpectrum(1,0,0);
+% greenS =        toSpectrum(0,1,0);
+% blueS =         toSpectrum(0,0,1);
+
+im = imread('apples.jpg');
+imd = im2double(im);
+
+
+
+
+
